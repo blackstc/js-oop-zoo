d0aa75941d4fc790fb7edb66e686f11f9c911cc3
Blackstock <charlieblackstock@gmail.com>
2015-08-08 14:22:37 -0600

    added methods for Zoo class

--- src/zoo.js
+++ src/zoo.js
@@ -3,6 +3,37 @@ var Animal = require("./animal");
 function Zoo(name, location){
   this.name = name;
   this.location = location;
+  this.status = "closed";
+  this.animals = [];
 }
 
+Zoo.prototype.changeLocation = function (newLocation) {
+  return this.location = newLocation;
+};
+
+Zoo.prototype.open = function () {
+  return this.status = "open";
+};
+
+Zoo.prototype.close = function () {
+  return this.status = "closed";
+};
+
+Zoo.prototype.isOpen = function () {
+  if (this.status === "open") {
+    return "Open!";
+  }
+};
+
+Zoo.prototype.addAnimal = function (animal) {
+  if (this.status === "open") {
+    this.animals.push(animal);
+  }
+};
+
+Zoo.prototype.removeAnimal = function (animal) {
+  if (this.status === "open") {
+    this.animals.splice(this.animals.indexOf(animal), 1);
+  }
+};
 module.exports = Zoo;
